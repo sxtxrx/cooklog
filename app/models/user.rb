c1488cef3fc5d8ff0b79169fb6129d381b1b2778
sxxrx <sxtxrxfor.pg@gmail.com>
2021-06-29 17:02:38 +0900

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,10 @@ class User < ApplicationRecord
                       def authenticated?(remember_token)
                         return false if remember_digest.nil?
                         BCrypt::Password.new(remember_digest).is_password?(remember_token)
-                      end                  
+                      end      
+                      def forget
+                        update_attribute(:remember_digest, nil)
+                      end            
                       
   private
       def downcase_email
