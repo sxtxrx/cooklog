b058896e28ad8b2c1d8c16545043135830c76990
sxxrx <sxtxrxfor.pg@gmail.com>
2021-06-29 17:30:40 +0900

    test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,16 +14,25 @@ module SessionsHelper
     end
   
     def log_out
+      forget(current_user) 
       session.delete(:user_id)
       @current_user = nil
     end
-
+    
     def remember(user)
       user.remember
       cookies.permanent.signed[:user_id] = user.id
       cookies.permanent[:remember_token] = user.remember_token
     end
+    
+    def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+    end
   
+  
+    # 記憶トークンcookieに対応するユーザーを返す
     def current_user
       if session[:user_id]
         @current_user ||= User.find_by(id: session[:user_id])
@@ -36,16 +45,6 @@ module SessionsHelper
           @current_user = user
         end
       end
-      def forget(user)
-        user.forget
-        cookies.delete(:user_id)
-        cookies.delete(:remember_token)
-      end
-    
-      def log_out
-        forget(current_user)  # 追記
-        session.delete(:user_id)
-        @current_user = nil
-      end
-  
   end
+end
+end
